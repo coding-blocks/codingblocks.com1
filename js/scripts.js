2ddd05b598bc13b9550e992184fbb02582266bf2
Arnav Gupta <arnav@codingblocks.com>
2017-04-04 15:40:56 +0530

    add chat
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1370,6 +1370,13 @@ function prepareSignup(iFrame){
 
 }
 
+//<!-- BEGIN JIVOSITE CODE {literal} -->
+//<script type='text/javascript'>
+    (function(){ var widget_id = 'mm3xh9v8wk';var d=document;var w=window;function l(){
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+//    </script>
+//<!-- {/literal} END JIVOSITE CODE -->
+
 
 
 /*\
