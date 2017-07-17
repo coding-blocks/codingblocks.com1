acc025da5dfd3c989dc58f2261d2f2aed899c96d
hereisnaman <naman@outlook.in>
2017-07-17 17:13:37 +0530

    Include Completed

--- gulp/tasks/include.js
+++ gulp/tasks/include.js
@@ -1,24 +1,24 @@
 let gulp = require('gulp')
 let inject = require('gulp-inject')
-
+let injectPartials = require('gulp-inject-partials')
 let config = require('../config.js').include
 
 gulp.task('include', ['envSetup'], function () {
     let json = require('../../' + config.includeSrc + config.name + '.json')
-    //json.include(  )
-    const promises = json.include.map(include => new Promise( (resolve, reject) => {
-        console.log(include.name)
-        const stream = gulp.src(config.hbsSrc + '/**/*.hbs')
+    let stream = gulp.src(config.hbsSrc + '/**/*.hbs')
+    json.include.map(include => {
+        stream = stream
             .pipe(inject(gulp.src(config.includeSrc + include.injectName + '.html'), {
-                starttag: '<!-- inject:head:{{ext}} -->',
+                name: include.injectName,
                 transform: function (filePath, file) {
-                    console.log(filePath + "\n" + file.contents.toString('utf8'))
                     return file.contents.toString('utf8')
                 }
             }))
+            .pipe(gulp.dest(config.hbsSrc))
+    })
+    return new Promise((resolve, reject) => {
         stream.on('finish', resolve)
         stream.on('error', reject)
-    }))
+    })
 
-    return Promise.all(promises)
 })
