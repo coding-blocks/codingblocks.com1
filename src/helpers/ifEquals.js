b673c9b3e07b838694731508afdae037210c751e
Naman Kumar <naman@outlook.in>
2017-11-23 22:47:20 +0530

    Structure based on course IDs

--- src/helpers/ifEquals.js
+++ src/helpers/ifEquals.js
@@ -1,4 +1,6 @@
 module.exports = function (str1, str2, options) {
+    if (!str1 || !str2)
+        return;
     if (str1.toString().toLowerCase() === str2.toString().toLowerCase())
         return options.fn(this);
 }
