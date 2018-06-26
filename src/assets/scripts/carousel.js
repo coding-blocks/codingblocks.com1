9555efe916dff545c96ddd4b4c565d247df8378f
fire-finch <firefinchdev@gmail.com>
2018-06-26 17:15:11 +0530

    Add: Online Carousel

---
+++ src/assets/scripts/carousel.js
@@ -0,0 +1,20 @@
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+    interval: 1000 * 2
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    if (next.next().length>0) {
+        next.next().children(':first-child').clone().appendTo($(this));
+    } else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+});
