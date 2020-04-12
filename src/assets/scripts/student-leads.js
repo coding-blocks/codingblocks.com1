c8868511d1975fff2c821532880439601428a63c
vdvibhu20 <vibhu.dujari@codingblocks.com>
2020-04-12 12:41:02 +0530

    regex for mobile number

--- src/assets/scripts/student-leads.js
+++ src/assets/scripts/student-leads.js
@@ -1,4 +1,17 @@
 $(function() {
+  var $regexname = /^[0][1-9]\d{9}$|^[1-9]\d{9}$/g;
+  $('#phoneNumber').on('keypress keydown keyup', function () {
+    if (!$(this).val().match($regexname)) {
+      // there is a mismatch, hence show the error message
+      $('#invalid-phone-number-error').removeClass('display-none');
+      $('#invalid-phone-number-error').show();
+    }
+    else {
+      // else, do not display message
+      $('#invalid-phone-number-error').addClass('display-none');
+    }
+  });
+
   $('#student-lead-form').submit(function (e) {
     $('#otp-error').addClass('display-none')
     $('#invalid-otp-error').addClass('display-none')
@@ -6,22 +19,21 @@ $(function() {
     e.preventDefault();
 
     var form = $(this)
-    var url = "https://app.codingblocks.com/student_leads"
+    var url = form.attr('action')
 
     var request = $.ajax({
       method: "POST",
       url: url,
-      data: form.serialize()
+      data: form.serialize(),
+      json: true
     })
     
     request.done(function(response) { 
-      $('#student-lead-form').addClass('display-none')
+      $('#lead-form').addClass('display-none')
       $('#student-lead-form-success').removeClass('display-none')
     })
       
     request.fail(function (xhr, textStatus, errorThrown) {
-      debugger
-      console.log(xhr, textStatus, errorThrown)
         if (xhr.status == 403) {
           $('#invalid-otp-error').removeClass('display-none')
         } else if (xhr.responseJSON.name == "SequelizeUniqueConstraintError") {
@@ -35,6 +47,11 @@ function requestOtp() {
   $('#otp-error').addClass('display-none')
   $('#invalid-otp-error').addClass('display-none')
   $('#invalid-mobile-error').addClass('display-none')
+
+  var $regexname = /^[0][1-9]\d{9}$|^[1-9]\d{9}$/g;
+  if (!$('#phoneNumber')[0].value.match($regexname)) {
+    return
+  }
 
   var request = $.ajax('https://app.codingblocks.com/student_leads/get_otp', {
     method: "POST",
