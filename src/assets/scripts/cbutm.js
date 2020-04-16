0295c9ecebd9cb47ed1eff70fc7f51033ffa78ab
Arnav Gupta <arnav@codingblocks.com>
2020-04-16 06:09:55 +0530

    CB UTM in cookie script
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ src/assets/scripts/cbutm.js
@@ -0,0 +1,30 @@
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/" + "; SameSite=Lax";
+}
+function saveUtmParamsToCookie() {
+    if (window.location.search) {
+        const params = new URLSearchParams(window.location.search);
+        const cbutm = {};
+        [
+            "utm_campaign",
+            "utm_source",
+            "utm_medium",
+            "utm_keyword",
+            "utm_content",
+            "utm_coupon",
+        ].forEach((key) => {
+            if (params.get(key)) {
+                cbutm[key] = params.get(key);
+            }
+        });
+        const cookieValue = btoa(JSON.stringify(cbutm));
+        setCookie('_cbutm', cookieValue, 7)
+    }
+}
+window.addEventListener("DOMContentLoaded", saveUtmParamsToCookie);
